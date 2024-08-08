fe576f0fcc9465b737f117b568907cd9e0167ec0
Chase Manning <chase@manning.dev>
2024-08-08 23:01:29 +0100

    :sparkles: add button component

---
+++ website/src/components/Button.tsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+const StyledButton = styled.a<{ $primary: boolean }>`
+  padding: 1rem 2rem;
+  border-radius: 0.5rem;
+  font-size: 1.8rem;
+  font-weight: 500;
+  min-width: 15rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  color: ${({ $primary }) =>
+    $primary ? "var(--primary)" : "var(--secondary)"};
+  border: 2px solid
+    ${({ $primary }) => ($primary ? "var(--primary)" : "var(--secondary)")};
+`;
+
+interface Props {
+  children: React.ReactNode;
+  href: string;
+  primary?: boolean;
+}
+
+const Button = ({ children, href, primary = false }: Props): JSX.Element => {
+  return (
+    <StyledButton href={href} $primary={primary} target="_blank">
+      {children}
+    </StyledButton>
+  );
+};
+
+export default Button;
