84ae0e4df01e12b8d87c0ddacdfa086bf56fb8eb
Chase Manning <chase@manning.dev>
2024-08-12 14:04:53 +0100

    :rocket: add cross origin request support

--- api/functions/index.js
+++ api/functions/index.js
@@ -1,16 +1,21 @@
 const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+const cors = require("cors");
+
+const corsFunc = cors({ origin: true });
 
 const REPO = "chase-manning/open-dog-registry";
 const DATA_URL = `https://raw.githubusercontent.com/${REPO}/main/data/v1.json`;
 
 exports.v1 = onRequest(async (request, response) => {
-  try {
-    const res = await fetch(DATA_URL);
-    const data = await res.json();
-    response.status(200).send({ status: "success", data });
-  } catch (error) {
-    logger.error(error);
-    response.status(500).send({ status: "error", error });
-  }
+  corsFunc(request, response, async () => {
+    try {
+      const res = await fetch(DATA_URL);
+      const data = await res.json();
+      response.status(200).send({ status: "success", data });
+    } catch (error) {
+      logger.error(error);
+      response.status(500).send({ status: "error", error });
+    }
+  });
 });
